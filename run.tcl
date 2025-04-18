848ed72e96e5af761daf7aba192638be61e2bfdb
shyamvaaluka <shyam.gudikandula@vaaluka.com>
2025-04-18 13:37:29 +0530

    tcl

--- run.tcl
+++ run.tcl
@@ -3,7 +3,6 @@ rm -rf work/
 vlog -sv -cover bcst  package2.sv trans_pkg.sv package.sv  top.sv 
 vsim -gui -coverage -assertdebug -voptargs=+acc -onfinish stop work.top +TEST0
 #vsim -voptargs=+acc work.top
-do wave_latency_sv_tb.do
 run -all
 coverage save -assert -directive -cvg -codeAll cov.ucdb
 vcover report -html -output covhtmlreport -details -assert -directive -cvg -code bcefst -threshL 50 -threshH 90 cov.ucdb
